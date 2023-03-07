02a79526b40d3306aebcfed2b6d7a65f9a6883c1
himaja-poluri <himajapoluri@gmail.com>
2023-03-07 10:45:47 +0530

    added class component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React,{ Component} from 'react'
+import Web3 from 'web3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//           Hello world!!!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+class App extends Component {
+
+  // async loadBlockchainData()
+  // {
+    
+  // }
+
+
+  render()
+  {
+    return(
+      <div className='container'>
+        <h1>Hello,himaja</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
